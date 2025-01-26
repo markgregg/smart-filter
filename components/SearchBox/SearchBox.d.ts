74fd8d7a05dd37ee77324f6add19d54cec76333f
Mark Gregg <gregg.mark@gmail.com>
2025-01-26 10:05:55 +0000

    Updates

--- components/SearchBox/SearchBox.d.ts
+++ components/SearchBox/SearchBox.d.ts
@@ -1,12 +1,13 @@
 import { default as React } from '../../../node_modules/react';
-import { Field, Option } from '../../../../../../../src/types';
+import { Brackets, Field, Option } from '../../../../../../../src/types';
 
 interface SearchBoxProps {
     matcherKey?: string;
     field?: Field;
     text: string[];
     onSelect: (option: Option) => void;
+    onCreateBracket?: (bracket: Brackets) => void;
     position?: number;
 }
-export declare const SearchBox: React.MemoExoticComponent<({ matcherKey, field, text, onSelect, position }: SearchBoxProps) => import("react/jsx-runtime").JSX.Element>;
+export declare const SearchBox: React.MemoExoticComponent<({ matcherKey, field, text, onSelect, onCreateBracket, position, }: SearchBoxProps) => import("react/jsx-runtime").JSX.Element>;
 export {};
